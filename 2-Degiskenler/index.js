e1ce2b077e398722e606596be2605c0a646aa322
Ebubekir71 <tosunbekir91@gmail.com>
2023-08-02 18:49:19 +0200

    degiskenler

---
+++ 2-Degiskenler/index.js
@@ -0,0 +1,14 @@
+let ad="Emin";
+let soyad="Basbayan";
+let müdürAdi="Ahmet";
+let sinifBaskani="Ebubekir";
+
+console.log(ad+" "+soyad);
+console.log("Müdür Adi:" + müdürAdi);
+console.log("Sinif Baskani:"+ sinifBaskani);
+console.log("Sunum yapacak Kisiler", müdürAdi, sinifBaskani);
+
+const maas=7000;
+const zam=0.1;
+let maasliZam= maas+maas*zam
+console.log(maasliZam);
