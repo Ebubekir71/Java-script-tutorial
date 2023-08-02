c15345ed152bbea733531a2b6370dc4606cf766b
Ebubekir71 <tosunbekir91@gmail.com>
2023-08-02 19:37:45 +0200

    degisken türleri

---
+++ 3-Degisken-Türleri/index.js
@@ -0,0 +1,28 @@
+/*---string---*/
+
+// let ogrenciIsmi="Ahmet Yilmaz";
+// let ogrenciYasi='25';
+// console.log(typeof ogrenciIsmi);
+
+/*---number---*/
+
+// let sayi1='10';
+// let sayi2='20';
+// let toplam= Number (sayi1) + Number (sayi2);
+// let toplam = Number(sayi1) + Number(sayi2);
+// let toplam =
+// console.log(toplam);
+
+
+/*---Boolen---*/
+
+//console.log(typeof false);
+//console.log(typeof true);
+// let sayi1 =10;
+// let sayi2 =20;
+// console.log(sayi1 < sayi2 );
+// console.log(sayi1 > sayi2);
+
+/*--undefined--*/
+// let ogrenciIsmi
+// console.log(typeof ogrenciIsmi);
