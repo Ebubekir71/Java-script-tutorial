e75cd57925fdf105eeae52f5379d99d3b7834626
Ebubekir71 <tosunbekir91@gmail.com>
2023-08-03 15:09:54 +0200

    if-else-1

---
+++ 6-If-else/index.js
@@ -0,0 +1,22 @@
+
+const username = 'ebubekir71'
+const password = '12345'
+
+if(username =='ebubekir71'){
+    if(password === '12345'){
+    console.log('giris yapildi');
+    }else{
+        console.log('sifre yanlis');
+    }
+}else{
+    console.log('Kullanici bulunamadi.');
+}
+
+
+// let isLoggedIn = false
+// if(isLoggedIn == true){
+//     console.log("calisti");
+// }else{
+//     console.log("true degil");
+// }
+// console.log('cikti!');
