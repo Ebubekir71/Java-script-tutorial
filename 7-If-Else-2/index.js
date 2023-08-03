dfbd569e1d5544846eee0285a69bc93e31d78145
Ebubekir71 <tosunbekir91@gmail.com>
2023-08-03 19:23:33 +0200

    string

---
+++ 7-If-Else-2/index.js
@@ -0,0 +1,22 @@
+// let yas = 17;
+// let mezuniyet = 'üniversite';
+
+// if((yas>=18) && (mezuniyet == 'lise' || mezuniyet=='üniversite')){
+//     console.log('Ehliyet sartlarini karsiladigin icin ehliyet alabilirsin');
+// }else if(yas===17){
+//     console.log('Dayan son bir senen.');
+
+// }else{
+//     console.log('Ehliyet sartlarini karsilamadigin icin ehliyet alamazsin');
+// }
+
+let yasi = 10;
+let mezuniyet = 'üniversite'
+if ((yasi>= 18) && (mezuniyet = 'üniveresite')){
+console.log('Ehliyet sartlarini karsiladigin icin alabilirsin');
+}else if(yasi===17){
+    console.log("dayan bir senen kaldi");
+}else if(yasi<=17){
+    console.log('daha cok ekmek emen lazim');
+}
+
---
+++ 8-String/index.js
@@ -0,0 +1,13 @@
+const ad ='Ebubekir';
+const soyad = "Tosun";
+const yas = 32;
+const sehir = "Bern";
+const meslek = "Full-Stack developer";
+const bio1 ="Benim adim" + " " + ad  + " " + "soyadim"+ " " + soyad+ ". " +sehir + "'de yasiyorum."+"."+"Yasim "+ yas+".";
+
+console.log(bio1);
+
+
+/*--java backtick --*/ 
+const newBio = `Benim adim ${ad} soyadim ${soyad}. ${sehir}'de yasiyorum. ${yas} yasimdayim.${meslek} olarak calisiyorum.`
+console.log(newBio);
