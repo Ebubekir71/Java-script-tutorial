5d62ecd7cdf20233bbd311b4e527aaae2de0ef6b
Ebubekir71 <tosunbekir91@gmail.com>
2023-08-04 13:08:04 +0200

    string-metodlari

---
+++ 9-String-Metodlari/index.js
@@ -0,0 +1,11 @@
+let youtube ="Bilgisayar genetigi";
+
+let result;
+result = youtube.toLowerCase();
+result = youtube.toUpperCase();
+result = youtube.length;
+result = youtube.trim();
+result = youtube.slice(0,6);
+result = youtube.split(' ');
+
+console.log(result);
