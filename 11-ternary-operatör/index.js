2154f0c4258c7db757c1589f66a1e7d3cb3a4804
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-06 13:28:55 +0200

    ternary operatör

---
+++ 11-ternary-operatör/index.js
@@ -0,0 +1,12 @@
+const username = "ebubekir tosun";
+const password = "123456";
+
+
+password.length > 6 ? console.log("calisti"): console.log("6'dan büyük olmadigi icin calismaz");
+username.length > 14 ? console.log("usernamedogru"): console.log("dogru degil");
+
+// if(password.length > 6){
+//     console.log("calisti");
+// }else{
+//     console.log("6'dan büyük olmadigi icin calismaz");
+// }
