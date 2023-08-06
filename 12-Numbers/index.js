11540e323b743a7daba3a40250c6dd640a8e2847
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-06 14:37:54 +0200

    numbers

---
+++ 12-Numbers/index.js
@@ -0,0 +1,25 @@
+const username = "ebubekir tosun";
+const yas = 25;
+
+let result;
+result =  25;
+result = Number("25");
+result = parseInt("25.7");
+result = parseFloat("25.7");
+result = parseInt("27a")
+
+let myNumber = 10.354654654;
+result = myNumber.toPrecision(5);
+result = myNumber.toFixed(4);
+
+result = Math.round(1.4);
+result = Math.round(1.6);
+result = Math.ceil(1.2);
+result = Math.floor(1.2);
+result = Math.pow(5, 3);
+result = Math.sqrt(25);
+result = Math.abs(-25);
+result = Math.max(21, 5, 3, 65);
+result = Math.min(21, 5, 9, 65);
+
+console.log(result);
