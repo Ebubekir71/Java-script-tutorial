221490444474296c587e1e650603706c328f5ee6
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-09 21:15:07 +0200

    dizinler uygulama

---
+++ 16-diziler-uygulama/index.js
@@ -0,0 +1,12 @@
+let customer = ["emin", "nur", "celal"];
+let customer1=["emin", ["asus", "monster", "dell",[10, 20, 30]]];
+
+console.log(`urun: ${customer1[1][0]} fiyati : ${customer1[1][3][0]}`);
+
+//dizinin ilk elemanini bulma
+
+console.log(customer[0]);
+
+// dizinin son elemanini bulma
+
+console.log(customer[customer.length-1]);
