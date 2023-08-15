1f8b90e494d475cdf208f9d6496490ba57b898cf
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-15 22:01:48 +0200

    döngüler

---
+++ 18-Döngüler/index.js
@@ -0,0 +1,10 @@
+let meyveler= ["elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun","elma", "armut", "kelek", "kavun",];
+
+for (let i = 0; i < meyveler.length; i++) {
+    console.log(meyveler[i]);
+    
+}
+for (let i = 0; i <= 100; i= i + 2) {
+    console.log(i);
+    
+}
