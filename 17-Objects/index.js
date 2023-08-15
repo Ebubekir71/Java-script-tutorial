15a8bd53e5b1cf992ea5a16111d54911b14b3445
Ebubekir Tosun <tosunbekir91@gmail.com>
2023-08-15 20:46:41 +0200

    objects

---
+++ 17-Objects/index.js
@@ -0,0 +1,36 @@
+let customer = {
+    "ad": "Emin",
+    "soyad": "basbayan",
+    "sehir": "kirikkale",
+    "yas": 25,
+    "produckts": [
+        "laptop","tablet", "araba", "phone"
+    ]
+};
+let customer1 = {
+    "ad": "Ebubekir",
+    "soyad": "Kalkan",
+    "sehir": "kirikkale",
+    "yas": 21,
+    "produckts":
+        {
+            "phone": "iphone",
+            "price": 150,
+        }
+};
+let customer2 = {
+    "ad": "Mahmut",
+    "soyad": "Ilhan",
+    "sehir": "kirikkale",
+    "yas": 35,
+    "products":
+    [
+        "araba", "villa", "mercedes"
+    ]
+};
+let musteriler = [
+    customer,
+    customer1,
+    customer2,
+]
+console.log(musteriler);
